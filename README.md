878f65c6f4adf72781d16f9cbb1fda3ad8257ef9
stynan27 <seamustynan16@gmail.com>
2024-11-02 11:31:08 -0400

    Contact /mytrades

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Description
 
 The goal of this project is to retrieve crypto financial data from multiple brokerage platforms. Allowing the user to aggregate the transactions and calculate a running cost basis given an AsOf date.
+
+## API Access
+
+https://exchange.gemini.com/settings/api
--- totals.py
+++ totals.py
@@ -1,17 +1,53 @@
 import requests, json
+import time
+import base64
+import hmac
+import hashlib
 
+# CONSTANTS
 BTC_TOKEN = 'BTC'
 ETH_TOKEN = 'ETH'
 
 # spreadsheets instead of API? -Access may be difficult to attain.
-BASE_URL = "https://api.gemini.com/v1"
+BASE_URL = "https://api.gemini.com"
 
-def network_info(token):
-    # Retrieve Blockchain Network Info
-    response = requests.get(BASE_URL + "/network/" + token)
+API_KEY = ''
+API_SECRET = ''
+with open('secrets.json', 'r') as file: 
+    data = json.load(file)
+    API_KEY = data['API_KEY'].encode()
+    API_SECRET = data['API_SECRET'].encode()
+    
+# Current time in seconds
+PAYLOAD_NONCE = time.time()
+
+def trade_info(token):
+    payload =  {
+        "request": "/v1/mytrades", 
+        "nonce": PAYLOAD_NONCE,
+    }
+    
+    encoded_payload = json.dumps(payload).encode()
+    b64 = base64.b64encode(encoded_payload)
+    signature = hmac.new(API_SECRET, b64, hashlib.sha384).hexdigest()
+
+    request_headers = {
+        'Content-Type': "text/plain",
+        'Content-Length': "0",
+        'X-GEMINI-APIKEY': API_KEY,
+        'X-GEMINI-PAYLOAD': b64,
+        'X-GEMINI-SIGNATURE': signature,
+        'Cache-Control': "no-cache"
+    }
+    
+    response = requests.post(
+        url=BASE_URL+"/v1/mytrades", 
+        headers=request_headers,
+        json=payload
+    )
 
     print("Response:")
     print(response.json())
     
     
-network_info(ETH_TOKEN)
+trade_info(ETH_TOKEN)
